4dbf8e85ae27dfeecded0b05389f6580bb115d85
Nikita <emper137137@gmail.com>
2021-12-19 11:21:32 +1000

    C:/Program Files/Git/api/users and /api/auth backend

---
+++ routes/api/users.js
@@ -0,0 +1,82 @@
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+// Item Model
+const User = require('../../models/User');
+
+// @route           POST api/users
+// @description     Register new user
+// @access          Public
+router.post('/', (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res
+            .status(400)
+            .json({ message: 'Please, fill out all fields' });
+    }
+
+    User.findOne({ email })
+        .then(user => {
+            if (user) return res.status(400).json({ message: 'User already exists' });
+
+            const newUser = new User({
+                name,
+                email,
+                password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+
+                    newUser.password = hash;
+
+                    newUser.save().then(user => {
+                        jwt.sign(
+                            { id: user._id },
+                            process.env.JWT_SECRET,
+                            { expiresIn: 1800 },
+                            (err, token) => {
+                                if (err) throw err;
+                                res.status(200).json({
+                                    token,
+                                    user: {
+                                        id: user._id,
+                                        name: user.name,
+                                        email: user.email
+                                    }
+                                });
+                            }
+                        );
+                    });
+                });
+            })
+        })
+        .catch(err => console.log(err));
+});
+
+// @route           GET api/users
+// @description     GET All Users
+// @access          Private
+router.get('/', (req, res) => {
+    User
+        .find()
+        .sort({ date: 1 })
+        .then(users => res.json(users))
+});
+
+// @route           DELETE api/users/:id
+// @description     Delete An Item
+// @access          Public
+router.delete('/:id', (req, res) => {
+    Item
+        .findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ id: req.params.id })))
+        .catch(e => res.status(404).json({success: false}));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config({ path: './config/.env' });
-
-const items = require('./routes/api/items');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
-
-const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fmalu.mongodb.net/mern-shopping-list?retryWrites=true&w=majority`;
+app.use(express.json());
 
 mongoose
-    .connect(mongoURI)
+    .connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fmalu.mongodb.net/mern-shopping-list?retryWrites=true&w=majority`)
     .then(() => console.log('MongoDB connected'))
     .catch(e => console.log(e));
 
 // Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets in PRODUCTION
 if (process.env.NODE_ENV === 'production') {
@@ -31,5 +28,4 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`server started on ${port}`));
